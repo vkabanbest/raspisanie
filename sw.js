5c3be39651b92dbf28d4b1352898d0b367d3b89b
vkabanbest <vkaban13@gmail.com>
2026-09-22 15:43:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'raspisanie-v11';
+const CACHE_NAME = 'raspisanie-v12';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
